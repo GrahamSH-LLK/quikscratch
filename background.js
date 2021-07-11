3a292fc46702db7e262158fc121fb123ce317eb5
GrahamSH <grahamshllk@gmail.com>
2021-07-11 10:39:44 -0400

    Awesome stuff

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.omnibox.setDefaultSuggestion({
-  description: "Type a project ID (prefixed with !), studio (prefixed with $), or username (prefixed with @)"
+  description:
+    "Type a project ID (prefixed with !), studio (prefixed with $), or username (prefixed with @)",
 });
 chrome.omnibox.onInputStarted.addListener(() => {
   console.log("User started omnibox!");
@@ -9,11 +10,11 @@ chrome.omnibox.onInputEntered.addListener(async (text, disposition) => {
   console.log("User accepted what was entered:\n", text);
   let url;
   if (text.startsWith("!")) {
-    url = `https://scratch.mit.edu/projects/${text.replace('!', '')}`;
+    url = `https://scratch.mit.edu/projects/${text.replace("!", "")}`;
   } else if (text.startsWith("@")) {
-    url = `https://scratch.mit.edu/users/${text.replace('@', '')}`;
+    url = `https://scratch.mit.edu/users/${text.replace("@", "")}`;
   } else if (text.startsWith("$")) {
-    url = `https://scratch.mit.edu/studios/${text.replace('$', '')}`;
+    url = `https://scratch.mit.edu/studios/${text.replace("$", "")}`;
   } else if (text == "") {
     url = `https://scratch.mit.edu/`;
   } else {
@@ -21,27 +22,13 @@ chrome.omnibox.onInputEntered.addListener(async (text, disposition) => {
   }
   switch (disposition) {
     case "currentTab":
-      chrome.tabs.update({url});
+      chrome.tabs.update({ url });
       break;
     case "newForegroundTab":
-      chrome.tabs.create({url});
+      chrome.tabs.create({ url });
       break;
     case "newBackgroundTab":
-      chrome.tabs.create({url, active: false});
+      chrome.tabs.create({ url, active: false });
       break;
   }
 });
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["popup.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  mode: "jit",
+};
